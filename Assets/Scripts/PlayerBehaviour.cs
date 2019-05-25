414b744656774f6f379ba18f7f0764bb12dcd0a1
Andrzej Łukasik <andrew.r.lukasik@gmail.com>
2019-05-25 10:16:27 +0200

    upload

---
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerBehaviour : NetworkBehaviour
+{
+
+    [SerializeField] GameObject _canvasPrefab = null;
+    GameObject _canvasInstance;
+
+
+    public override void OnStartLocalPlayer ()
+    {
+        _canvasInstance = Instantiate( _canvasPrefab , transform );
+    }
+    
+    [Command]
+    public void SendTexture ()
+    {
+        
+    }
+
+}
---
+++ Assets/Scripts/ServerBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerBehaviour : NetworkBehaviour
+{
+    
+    [SerializeField] GameObject _canvasPrefab = null;
+    GameObject _canvasInstance;
+
+    public override void OnStartServer ()
+    {
+        _canvasInstance = Instantiate( _canvasPrefab );
+    }
+    
+}
